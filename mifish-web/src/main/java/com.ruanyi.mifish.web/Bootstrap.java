01104fb3d822807c59aa0625fd2fc59544807dba
ruanyi <ruanlongsheng@foxmail.com>
2020-08-02 15:46:31 +0800

    提交一些基础代码

--- mifish-web/src/main/java/com.ruanyi.mifish.web/Bootstrap.java
+++ mifish-web/src/main/java/com.ruanyi.mifish.web/Bootstrap.java
@@ -1,5 +1,8 @@
 package com.ruanyi.mifish.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Description:
  *
