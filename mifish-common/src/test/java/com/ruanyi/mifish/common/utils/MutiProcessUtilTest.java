a6ebf9a70db63a4e024aa84f9bc2962f5ed44cfa
rls <ruanlongsheng@foxmail.com>
2023-08-31 10:57:22 +0800

    add commit

--- mifish-common/src/test/java/com/ruanyi/mifish/common/utils/MutiProcessUtilTest.java
+++ mifish-common/src/test/java/com/ruanyi/mifish/common/utils/MutiProcessUtilTest.java
@@ -17,7 +17,7 @@ public class MutiProcessUtilTest {
         String pid = UUIDUtil.obtainUUID();
         String ffmpeg =
             "/usr/local/bin/ffmpeg -i /Users/rls/Documents/tmp/watermark/f343da361cc3d1f57a960632bf208e06.mov -f lavfi -i \"color=black@0:s=300x300,format=yuva420p\" -filter_complex \"[1]trim=end_frame=1,drawtext=fontfile='PingFang SC':text='美图内部课程':fontsize=36:fontcolor=white:alpha=0.2,split[text][alpha];[text][alpha]alphamerge,rotate=(-30*PI/180):ow=rotw(-30*PI/180):oh=roth(-30*PI/180):c=black@0,loop=-1:1:0,tile=30x9,trim=end_frame=1[wm];[0][wm]overlay=0:0\" -y /Users/rls/Documents/tmp/watermark/output_9.mp4";
-        int code = MutiProcessUtil.runSubProcess(pid, 1, TimeUnit.MINUTES, "sh", "-c", "ls -lah");
+        int code = MutiProcessUtil.runSubProcess(pid, 1, TimeUnit.MINUTES, "sh", "-c", ffmpeg);
         System.out.println(code);
     }
 }
